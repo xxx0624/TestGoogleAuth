5923112581a960c6b540233c026705e0b27da551
xxx0624 <xzheng0624@gmail.com>
2019-06-05 18:45:08 -0700

    update credentials

--- googleOauth2.ts
+++ googleOauth2.ts
@@ -1,6 +1,6 @@
 class googleOauth2 {
 static id: string = '545053256990-52m47d5kn2gst9ic1v21drlpqiduftie.apps.googleusercontent.com';
-static secret:string = 'jEgRVkOw5QFXXt_K6fD3CGs1';
+static secret:string = 'BqW8b2JcLzSmjmRmzs30DLge';
 
 }
 export default googleOauth2;
