9f23da19f4dddd83020c445d1913a697de0da4c7
xxx0624 <xzheng0624@gmail.com>
2019-06-05 18:55:12 -0700

    update tsc config

--- googleOauth2.js
+++ googleOauth2.js
@@ -4,7 +4,7 @@ var googleOauth2 = /** @class */ (function () {
     function googleOauth2() {
     }
     googleOauth2.id = '545053256990-52m47d5kn2gst9ic1v21drlpqiduftie.apps.googleusercontent.com';
-    googleOauth2.secret = 'jEgRVkOw5QFXXt_K6fD3CGs1';
+    googleOauth2.secret = 'BqW8b2JcLzSmjmRmzs30DLge';
     return googleOauth2;
 }());
 exports["default"] = googleOauth2;
